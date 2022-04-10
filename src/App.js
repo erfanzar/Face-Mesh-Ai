871f0cc24ce656bd96d4f41aeaa644cbb0a1f77a
erfanzar <erfanzare82@yahoo.com>
2022-04-10 18:02:47 +0430

    created

--- src/App.js
+++ src/App.js
@@ -3,21 +3,28 @@ import './App.css';
 import React , {useEffect, useRef, useState} from 'react';
 import Webcam from 'react-webcam';
 import * as tf from '@tensorflow/tfjs';
-import * as model from '@tensorflow-models/face-landmarks-detection';
-
+import * as model from '@tensorflow-models/pose-detection'
+import glass from './png_glasses_31208.png';
 const App = ()=>{
 
   const webcamref = useRef(null);
 
   // console.log(webcamref);
+  const wh = window.innerHeight;
+  const ww = window.innerWidth;
 
+  const [x , setx] = useState(null);
+  const [y , sety] = useState(null);
+  const [neto, setNet] = useState(null);
   const RunModel = async()=>{
 
-    const net = await model.load(
-      model.SupportedPackages.mediapipeFacemesh
-    );
-    console.log("model Loaded");
+    const mdl = model.SupportedModels.MoveNet;
+    const net = await model.createDetector(mdl);
 
+    console.log("model Loaded");
+    if (net != null){
+      setNet(1)
+    }
     setInterval(()=>{
       detect(net)
     } , 60)
@@ -32,9 +39,22 @@ const App = ()=>{
       const height = webcamref.current.video.videoHeight;
       const width  = webcamref.current.video.videoWidth;
 
-      const pred = await net.estimateFaces({input : video})
+      const pred = await net.estimatePoses(video)
+      
 
-      console.log(pred)
+      const www = ww/width
+      const wwh = wh/height
+      // console.log(pred)
+
+      if(pred.length > 0){
+        for (let o of pred){
+          console.log(o.keypoints)
+          const landmark = o.keypoints
+          setx(landmark[0].x*wwh)
+          sety(landmark[2].y*www)
+        }
+        
+      }
     }
   }
   useEffect(
@@ -42,15 +62,35 @@ const App = ()=>{
       RunModel();
     },[]
   )
-  return(
-    <div>
-      <Webcam className='Camera'
-        ref={webcamref}
-        height={480}
-        width={640}       
-      />
-    </div>
-  );
+  if (neto == null){
+    return(
+      <div className='loading'>
+        <p>
+          Loading ...
+        </p>
+      </div>
+    )
+  }
+  else{
+    return(
+      <div className='app'>
+        <div className='cont'>
+        <Webcam className='Camera'
+          ref={webcamref}
+          height={480}
+          width={640}       
+        />
+        
+          <div className='point' style={{left:`${x-wh/9.5}px` , top:`${y-ww/7}px`} }>
+            <img src={glass} style={{
+              height:wh/7,
+              width:ww/3,
+            }}/>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
